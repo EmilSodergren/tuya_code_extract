f1a51df879eac55b2dc0d54a49fdb28c5566c1b7
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2023-12-09 16:48:37 +0100

    Print correct stuff

--- transform.py
+++ transform.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 
-import os
+import sys
+import json
 import xml.etree.ElementTree as ET
 
 
-def main(args):
-    with open('codes.xml') as f:
-        content = ET.parse(f)
+def read_tuya_xml(file):
+    with open(file) as f:
+        root = ET.parse(f)
+        for elt in root.iter():
+            if elt.tag == 'string' and elt.attrib['name'].startswith('s_home_data'):
+                return elt.text.strip().replace('\\\\"', '"')
 
-        print(content)
+
+def map_tuya_to_k8sconf(data):
+    for d in data:
+        tuya_conf = {}
+        tuya_conf['id'] = d['devId']
+        tuya_conf['ip'] = ""
+        tuya_conf['key'] = d['localKey']
+        tuya_conf['name'] = ""
+        tuya_conf['version'] = ""
+        print(json.dumps(tuya_conf, indent=4), ',', sep='')
+
+
+def main(args):
+    content = read_tuya_xml('codes.xml')
+    data = json.loads(content)
+    # print(json.dumps(data, indent=2))
+    map_tuya_to_k8sconf(data.get('deviceRespBeen'))
 
 
 if __name__ == '__main__':
-    main(os.argv)
+    main(sys.argv)
